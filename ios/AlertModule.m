81bbe2bd7a4e95b530feeb3188e27e1c0d7a1b82
Engin Bolat <engi.bolat@gmail.com>
2024-03-19 04:43:57 +0300

    Native Alert Modules Added

---
+++ ios/AlertModule.m
@@ -0,0 +1,8 @@
+//
+//  AlertModule.m
+//  RNNativeModules
+//
+//  Created by Engin Bolat on 19.03.2024.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/AlertModule.swift
@@ -0,0 +1,8 @@
+//
+//  AlertModule.swift
+//  RNNativeModules
+//
+//  Created by Engin Bolat on 19.03.2024.
+//
+
+import Foundation
